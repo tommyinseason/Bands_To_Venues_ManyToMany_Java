8bad3c5a718146de1c3afac8ed354fc352175481
Tommy Jones <tommyinseason@yahoo.com>
2016-05-19 09:28:10 -0700

    band page displays name

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -47,7 +47,7 @@ public class AppTest extends FluentTest {
     submit(".btn");
     assertThat(pageSource()).contains("CBGB");
   }
-  
+
   @Test
   public void bandIsDeleted() {
     Band testBand = new Band("ACDC");
@@ -59,15 +59,15 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("$band.getName()");
   }
 
-  // @Test
-  // public void bandShowPageDisplaysName() {
-  //   Band testBands = new Band("ACDC");
-  //   testBands.save();
-  //   String url = String.format("http://localhost:4567/bands/%d", testBands.getId());
-  //   goTo(url);
-  //   assertThat(pageSource()).contains("ACDC");
-  // }
-  //
+  @Test
+  public void bandShowPageDisplaysName() {
+    Band testBands = new Band("ACDC");
+    testBands.save();
+    String url = String.format("http://localhost:4567/bands/%d", testBands.getId());
+    goTo(url);
+    assertThat(pageSource()).contains("ACDC");
+  }
+
   // @Test
   // public void venueShowPageDisplaysName() {
   //   Venue testVenue = new Venue("CBGB");
